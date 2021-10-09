01563ef6c68387d5f4e4fd2660606b11c2702536
AshutoshAwasthi-2078 <53912667+AshutoshAwasthi-2078@users.noreply.github.com>
2021-10-09 09:39:13 +0530

    Add files via upload

--- job/sequencing.cpp
+++ job/sequencing.cpp
@@ -1,65 +1,56 @@
-#include<bits/stdc++.h>
-using namespace std; 
-
-struct Job 
-{ 
-    int id;	 // Job Id 
-    int dead; // Deadline of job 
-    int profit; // Profit if job is over before or on deadline 
-}; 
-
-
-class Solution 
+#include<iostream>
+#include<algorithm>
+using namespace std;
+  
+struct Job
 {
-    public:
-    static bool Compare(Job a , Job b){
-        return a.profit>b.profit;
-    }
-    //Function to find the maximum profit and the number of jobs done.
-    vector<int> JobScheduling(Job arr[], int n) 
-    { 
-        sort(arr, arr+n, Compare);
-        bool done[n] = {0};
-        int day =0 , profit =0;
-        for(int i =0; i<n; i++){
-            for (int j=min(n, arr[i].dead-1); j>=0 ;j--){
-                if(done[j]==false){
-                    day+=1;
-                    profit+=arr[i].profit;
-                    done[j]=true;
-                    break;
-                }
-            }
-        }
-        return {day,profit};
-    } 
+   char id;     // Job Id
+   int dead;    // Deadline of job
+   int profit;  // Profit if job is over before or on deadline
 };
-
-int main() 
-{ 
-    int t;
-    //testcases
-    cin >> t;
-    
-    while(t--){
-        int n;
-        
-        //size of array
-        cin >> n;
-        Job arr[n];
-        
-        //adding id, deadline, profit
-        for(int i = 0;i<n;i++){
-                int x, y, z;
-                cin >> x >> y >> z;
-                arr[i].id = x;
-                arr[i].dead = y;
-                arr[i].profit = z;
-        }
-        Solution ob;
-        //function call
-        vector<int> ans = ob.JobScheduling(arr, n);
-        cout<<ans[0]<<" "<<ans[1]<<endl;
+  
+bool comparison(Job a, Job b)
+{
+     return (a.profit > b.profit);
+}
+  
+void printJobScheduling(Job arr[], int n)
+{
+    sort(arr, arr+n, comparison);
+  
+    int result[n]; // To store result (Sequence of jobs)
+    bool slot[n];  // To keep track of free time slots
+  
+    for (int i=0; i<n; i++)
+        slot[i] = false;
+  
+    for (int i=0; i<n; i++)
+    {
+       for (int j=min(n, arr[i].dead)-1; j>=0; j--)
+       {
+          
+          if (slot[j]==false)
+          {
+             result[j] = i;  
+             slot[j] = true;
+             break;
+          }
+       }
     }
-	return 0; 
+  
+
+    for (int i=0; i<n; i++)
+       if (slot[i])
+         cout << arr[result[i]].id << " ";
+}
+  
+int main()
+{
+    Job arr[] = { {'a', 2, 100}, {'b', 1, 19}, {'c', 2, 27},
+                   {'d', 1, 25}, {'e', 3, 15}};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "Maximum profit sequence of jobs \n";
+    
+    printJobScheduling(arr, n);
+    return 0;
 }
