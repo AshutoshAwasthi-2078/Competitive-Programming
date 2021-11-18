f82f4f361207ee61d5841eddadabfe6ce16b2991
AshutoshAwasthi-2078 <53912667+AshutoshAwasthi-2078@users.noreply.github.com>
2021-11-18 17:15:21 +0530

    Add files via upload

---
+++ tree/Problem14.c
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+
+ 
+struct node* newnode(int data)
+{
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+ 
+
+bool print_ancestors(struct node *root, int target)
+{
+    if (root == NULL)
+        return false;
+ 
+    if (root->data == target)
+        return true;
+ 
+    if ( printAncestors(root->left, target) || printAncestors(root->right, target) )
+    {
+        cout << root->data << " ";
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+
+  struct node *root = newnode(5);
+  root->left        = newnode(4);
+  root->right       = newnode(3);
+  root->left->left  = newnode(2);
+  root->left->right = newnode(1);
+  root->left->left->left  = newnode(6);
+ 
+  print_ancestors(root, 1);
+  return 0;
+}
